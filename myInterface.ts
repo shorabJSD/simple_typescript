ce78b3c8b147588c059c0ae13217dc3ccd4a8064
shorab akka <shorabakka6004@gmail.com>
2024-06-22 00:03:14 +0600

    add some code

--- myInterface.ts
+++ myInterface.ts
@@ -9,10 +9,23 @@ interface User {
     //couponname: string, value: number // it can be return anything else in the list of getCoupon methods for the user to get the coupon information from the user and the user should be able to get the coupon information from the user;
 }
 
+// create a new coupon information object basis on the User object and get back before which we have assinged valuee in the newUser object;
+interface User {
+    gitHubToken: string
+}
+
+//  create admin user object
+interface Admin extends User {
+    role: "admin" | "ta"  | "learner"
+}
+
+
 //assing values according above to the User object;
-const newUser: User = {
+const newUser: Admin ={
     bdId:2211, // you cant change the bdId here because the user is already in the process of creating a new user.
+    gitHubToken: "world is a battle ground for everyone ",
     userName: "sourov",
+    role: "admin",
     userId: 123,
     startTrail: ()=>{
         return 12
