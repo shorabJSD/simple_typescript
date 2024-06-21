b5ed7fa8f54f9cce80725f42298421d540987dae
shorab akka <shorabakka6004@gmail.com>
2024-06-21 21:11:56 +0600

    add new code

---
+++ myEnums.js
@@ -0,0 +1,8 @@
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice[SeatChoice["ASILE"] = 12] = "ASILE";
+    SeatChoice[SeatChoice["MIDDLE"] = 13] = "MIDDLE";
+    SeatChoice["WINDOW"] = "WINDOW";
+    SeatChoice["FOUTH"] = "FOUTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.MIDDLE;
---
+++ myEnums.ts
@@ -0,0 +1,8 @@
+ enum SeatChoice{
+    ASILE = 12,
+    MIDDLE,
+    WINDOW = "WINDOW",
+    FOUTH = "FOUTH",
+ }
+
+ const hcSeat = SeatChoice.MIDDLE;
