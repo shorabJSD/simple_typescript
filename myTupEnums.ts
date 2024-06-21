d2daec947162f7b3993e7e58d9bbcfa373449be9
shorab akka <shorabakka6004@gmail.com>
2024-06-21 18:40:01 +0600

    hellwo world

---
+++ myTupEnums.ts
@@ -0,0 +1,10 @@
+//triple check
+// let  User: (string | number | boolean)[] = [1, 2];
+let user:[string , number, boolean];
+user = ["hello", 12, false];
+
+let rgb: [number, number, number] = [125, 2566, 522];
+
+type User = [number, string];
+let newUser: User = [1254, "sourov akka"];
+newUser[1] = "hc.com";
