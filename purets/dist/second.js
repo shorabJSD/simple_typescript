b1ec460a196877aab96f006cbb185d8007a1641f
shorab akka <shorabakka6004@gmail.com>
2024-06-22 12:23:05 +0600

    i have made some code into the second.ts file

--- purets/dist/second.js
+++ purets/dist/second.js
@@ -1,1 +1,19 @@
 "use strict";
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, veido) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.veido = veido;
+    }
+    createStory() {
+        console.log("Story was created by developer ");
+    }
+}
