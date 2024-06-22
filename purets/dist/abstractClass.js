63f5f989d947569573d17ee8f21f40b1f9995cbf
shorab akka <shorabakka6004@gmail.com>
2024-06-23 03:33:03 +0600

    add new code line

--- purets/dist/abstractClass.js
+++ purets/dist/abstractClass.js
@@ -1,1 +1,23 @@
 "use strict";
+class TakePhoto {
+    constructor(cameraMode, imageSize) {
+        this.cameraMode = cameraMode;
+        this.imageSize = imageSize;
+    }
+    getRealTime() {
+        //some complex calculations;
+        return 1000;
+    }
+}
+class InstagramApp extends TakePhoto {
+    constructor(cameraMode, imageSize) {
+        super(cameraMode, imageSize);
+        this.cameraMode = cameraMode;
+        this.imageSize = imageSize;
+    }
+    getSepia() {
+        console.log("Sepia is running here!");
+    }
+}
+let sourov = new InstagramApp("test", "test");
+sourov.getRealTime();
---
+++ purets/src/abstractClass.ts
@@ -0,0 +1,29 @@
+abstract class TakePhoto {
+    constructor(
+       public cameraMode: string,
+       public imageSize: string
+    ){}
+
+    abstract getSepia(): void
+    getRealTime(): number{
+        //some complex calculations;
+        return 1000;
+    }
+}
+
+class InstagramApp extends TakePhoto {
+     constructor(
+         public cameraMode: string,
+         public imageSize: string
+     ){
+       super(cameraMode, imageSize)
+     }
+    
+    getSepia(): void{
+        console.log("Sepia is running here!")
+    }
+
+}
+
+let sourov = new InstagramApp("test", "test");
+sourov.getRealTime();
